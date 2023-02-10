0ecbe3647e52456c3de071d054dc3be2d615b919
pvc2209 <cuongpv2209@gmail.com>
2023-02-11 00:55:52 +0700

    Handle submit code.

--- lib/utils/state_status_listener.dart
+++ lib/utils/state_status_listener.dart
@@ -4,17 +4,23 @@ import 'package:code_space_client/models/app_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-void stateStatusListener(BuildContext context, BaseState state) {
+void stateStatusListener(BuildContext context, BaseState state,
+    {VoidCallback? onSuccess}) {
   switch (state.stateStatus) {
     case StateStatus.loading:
       EasyLoading.show();
       break;
     case StateStatus.error:
+      EasyLoading.dismiss();
       if (state.error is NoNetworkException) {
         EasyLoading.showInfo(S.of(context).no_network);
       } else {
         EasyLoading.showInfo(state.error?.message ?? '');
       }
+      break;
+    case StateStatus.success:
+      EasyLoading.dismiss();
+      onSuccess?.call();
       break;
     default:
       EasyLoading.dismiss();
