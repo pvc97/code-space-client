7c60c170821f1d4bfbaf4e4ff0d808479d10149d
pvc2209 <cuongpv2209@gmail.com>
2023-02-13 00:03:58 +0700

    Add url.

--- lib/constants/url_constants.dart
+++ lib/constants/url_constants.dart
@@ -2,6 +2,7 @@ class UrlConstants {
   UrlConstants._();
   static const String refreshToken = '/auth/refresh-token';
   static const String login = '/auth/login';
-  static const String userInfo = '/user/user-info';
-  static const String submission = '/submission';
+  static const String userInfo = '/users/user-info';
+  static const String submission = '/submissions';
+  static const String problem = '/problems';
 }
