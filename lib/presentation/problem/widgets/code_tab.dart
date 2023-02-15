e4d59f3d069f6ece95d6396ad70ae56529dc7775
pvc2209 <cuongpv2209@gmail.com>
2023-02-15 23:01:19 +0700

    Highlight language.

--- lib/presentation/problem/widgets/code_tab.dart
+++ lib/presentation/problem/widgets/code_tab.dart
@@ -1,5 +1,7 @@
-import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:code_text_field/code_text_field.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
 
 class CodeTab extends StatefulWidget {
   final CodeController codeController;
@@ -20,9 +22,14 @@ class _CodeTabState extends State<CodeTab> with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return CodeField(
-      controller: widget.codeController,
-      expands: true,
+
+    // Have to wrap the CodeField in a CodeTheme to enable syntax highlighting
+    return CodeTheme(
+      data: const CodeThemeData(styles: monokaiSublimeTheme),
+      child: CodeField(
+        controller: widget.codeController,
+        expands: true,
+      ),
     );
   }
 }
