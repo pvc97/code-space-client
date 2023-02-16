0d320ac303d02feb06f7b1cca06bc634fcebf49c
pvc2209 <cuongpv2209@gmail.com>
2023-02-16 22:27:29 +0700

    Remove animation when change page in desktop.

--- lib/router/adaptive_transition_page.dart
+++ lib/router/adaptive_transition_page.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/foundation.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AdaptiveTransitionPage {
   AdaptiveTransitionPage._();
   static Page<dynamic> create(LocalKey? key, {required Widget child}) {
-    return kIsWeb
-        ? NoTransitionPage<void>(
+    return (Platform.isAndroid || Platform.isIOS)
+        ? MaterialPage<void>(
             key: key,
             child: child,
           )
-        : MaterialPage<void>(
+        : NoTransitionPage<void>(
             key: key,
             child: child,
           );
