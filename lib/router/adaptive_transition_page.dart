89f0bb66ad523585b9ff50bdec8617f1829b1fea
pvc2209 <cuongpv2209@gmail.com>
2023-02-19 23:28:48 +0700

    Apply cupertino page transition for desktop.

--- lib/router/adaptive_transition_page.dart
+++ lib/router/adaptive_transition_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -6,14 +7,19 @@ import 'package:go_router/go_router.dart';
 class AdaptiveTransitionPage {
   AdaptiveTransitionPage._();
   static Page<dynamic> create(LocalKey? key, {required Widget child}) {
-    return (kIsWeb || Platform.isWindows)
+    return (kIsWeb)
         ? NoTransitionPage<void>(
             key: key,
             child: child,
           )
-        : MaterialPage<void>(
-            key: key,
-            child: child,
-          );
+        : (Platform.isAndroid)
+            ? MaterialPage<void>(
+                key: key,
+                child: child,
+              )
+            : CupertinoPage<void>(
+                key: key,
+                child: child,
+              );
   }
 }
