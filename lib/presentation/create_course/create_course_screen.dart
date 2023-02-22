f3a7814d96bf4fb4510f0aab6acf5d59782a5ce1
pvc2209 <cuongpv2209@gmail.com>
2023-02-22 19:40:57 +0700

    Navigate to create course screen.

---
+++ lib/presentation/create_course/create_course_screen.dart
@@ -0,0 +1,11 @@
+import 'package:code_space_client/presentation/create_course/create_course_view.dart';
+import 'package:flutter/material.dart';
+
+class CreateCourseScreen extends StatelessWidget {
+  const CreateCourseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CreateCourseView();
+  }
+}
---
+++ lib/presentation/create_course/create_course_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateCourseView extends StatefulWidget {
+  const CreateCourseView({super.key});
+
+  @override
+  State<CreateCourseView> createState() => CreateCourseViewState();
+}
+
+class CreateCourseViewState extends State<CreateCourseView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
