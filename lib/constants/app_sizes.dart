e266bb7991bb2085140b6fcd5891377d028a12ca
pvc2209 <cuongpv2209@gmail.com>
2023-02-26 20:37:19 +0700

    Show problem language.

--- lib/constants/app_sizes.dart
+++ lib/constants/app_sizes.dart
@@ -1,6 +1,8 @@
 /// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
 class Sizes {
   Sizes._();
+  static const s1 = 1.0;
+  static const s2 = 2.0;
   static const s4 = 4.0;
   static const s8 = 8.0;
   static const s12 = 12.0;
