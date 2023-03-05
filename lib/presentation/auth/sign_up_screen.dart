14ab2f5c2aed7c3f81ce03117dcf60cb6be4bf87
pvc2209 <cuongpv2209@gmail.com>
2023-03-05 14:16:38 +0700

    Update UI.

--- lib/presentation/auth/sign_up_screen.dart
+++ lib/presentation/auth/sign_up_screen.dart
@@ -1,7 +1,9 @@
 import 'package:code_space_client/blocs/auth/auth_cubit.dart';
 import 'package:code_space_client/blocs/base/base_state.dart';
+import 'package:code_space_client/constants/app_sizes.dart';
 import 'package:code_space_client/generated/l10n.dart';
 import 'package:code_space_client/presentation/common_widgets/adaptive_app_bar.dart';
+import 'package:code_space_client/presentation/common_widgets/app_elevated_button.dart';
 import 'package:code_space_client/presentation/common_widgets/box.dart';
 import 'package:code_space_client/utils/extensions/string_ext.dart';
 import 'package:code_space_client/utils/logger/logger.dart';
@@ -51,119 +53,127 @@ class _SignUpScreenState extends State<SignUpScreen> {
         title: Text(S.of(context).sign_up),
       ),
       body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 300.0),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: _autovalidateMode,
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: S.of(context).username,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return S.of(context).username_cannot_be_empty;
-                    }
+        child: Form(
+          key: _formKey,
+          autovalidateMode: _autovalidateMode,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(Sizes.s20),
+            child: Center(
+              child: SizedBox(
+                width: Sizes.s300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: S.of(context).username,
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return S.of(context).username_cannot_be_empty;
+                        }
 
-                    if (!value.isValidUserName()) {
-                      return S.of(context).username_only_alphanumeric;
-                    }
+                        if (!value.isValidUserName()) {
+                          return S.of(context).username_only_alphanumeric;
+                        }
 
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _username = value;
-                  },
-                ),
-                const Box(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: S.of(context).full_name,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return S.of(context).full_name_cannot_be_empty;
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _fullName = value;
-                  },
-                ),
-                const Box(height: 10),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: S.of(context).email,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return S.of(context).email_cannot_be_empty;
-                    }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _username = value;
+                      },
+                    ),
+                    const Box(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: S.of(context).full_name,
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return S.of(context).full_name_cannot_be_empty;
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _fullName = value;
+                      },
+                    ),
+                    const Box(height: 10),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: S.of(context).email,
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return S.of(context).email_cannot_be_empty;
+                        }
 
-                    if (!value.isValidEmail()) {
-                      return S.of(context).email_is_not_valid;
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _email = value;
-                  },
-                ),
-                const Box(height: 10),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: S.of(context).password,
-                  ),
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return S.of(context).password_cannot_be_empty;
-                    }
+                        if (!value.isValidEmail()) {
+                          return S.of(context).email_is_not_valid;
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _email = value;
+                      },
+                    ),
+                    const Box(height: 10),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: S.of(context).password,
+                      ),
+                      obscureText: true,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return S.of(context).password_cannot_be_empty;
+                        }
 
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _password = value;
-                  },
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _password = value;
+                      },
+                    ),
+                    const Box(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: S.of(context).confirm_password,
+                      ),
+                      obscureText: true,
+                      validator: (String? value) {
+                        if (_passwordController.text != value) {
+                          return S.of(context).passwords_do_not_match;
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _confirmPassword = value;
+                      },
+                    ),
+                    const Box(height: 16),
+                    BlocBuilder<AuthCubit, AuthState>(
+                      builder: (context, state) {
+                        return FractionallySizedBox(
+                          widthFactor: 0.7,
+                          child: AppElevatedButton(
+                            onPressed: state.stateStatus == StateStatus.loading
+                                ? null
+                                : _submit,
+                            text: S.of(context).sign_up,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                const Box(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: S.of(context).confirm_password,
-                  ),
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (_passwordController.text != value) {
-                      return S.of(context).passwords_do_not_match;
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _confirmPassword = value;
-                  },
-                ),
-                const Box(height: 16),
-                BlocBuilder<AuthCubit, AuthState>(
-                  builder: (context, state) {
-                    return ElevatedButton(
-                      onPressed: state.stateStatus == StateStatus.loading
-                          ? null
-                          : _submit,
-                      child: Text(S.of(context).sign_up),
-                    );
-                  },
-                ),
-              ],
+              ),
             ),
           ),
         ),
