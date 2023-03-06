04ff194e420dd4a41a2ed945fa82188ecc10058e
pvc2209 <cuongpv2209@gmail.com>
2023-03-07 00:06:39 +0700

    Change pushNamed to goNamed for several screen.

---
+++ lib/presentation/change_password/change_password_screen.dart
@@ -0,0 +1,11 @@
+import 'package:code_space_client/presentation/change_password/change_password_view.dart';
+import 'package:flutter/material.dart';
+
+class ChangPasswordScreen extends StatelessWidget {
+  const ChangPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ChangePasswordView();
+  }
+}
---
+++ lib/presentation/change_password/change_password_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswordView extends StatefulWidget {
+  const ChangePasswordView({super.key});
+
+  @override
+  State<ChangePasswordView> createState() => _ChangePasswordViewState();
+}
+
+class _ChangePasswordViewState extends State<ChangePasswordView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
