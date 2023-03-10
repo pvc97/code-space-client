38eba553c0a2bf4a3dfa00219b83ba9bfaf8b8ff
pvc2209 <cuongpv2209@gmail.com>
2023-03-10 08:07:50 +0700

    Add comment.

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,16 +1,15 @@
 class AppImages {
   const AppImages._();
 
+  // When add new image, consider add to cache in AppConfigManager.init
   static const String courseDescriptionBackground =
       'assets/images/course_description_background.jpg';
-
   static const String notFound = 'assets/images/not_found.png';
+  static const String student = 'assets/images/student.png';
+  static const String teacher = 'assets/images/teacher.png';
+  static const String manager = 'assets/images/manager.png';
 
   static const String top1 = 'assets/images/top1.json';
   static const String top2 = 'assets/images/top2.json';
   static const String top3 = 'assets/images/top3.json';
-
-  static const String student = 'assets/images/student.png';
-  static const String teacher = 'assets/images/teacher.png';
-  static const String manager = 'assets/images/manager.png';
 }
