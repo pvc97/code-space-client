90618a724d875dd62eceb897d2e028e1f24bb10d
pvc2209 <cuongpv2209@gmail.com>
2023-03-11 16:25:11 +0700

    Increase debounce duration.

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,6 +1,9 @@
 class AppConstants {
   AppConstants._();
 
-  static const int searchDebounceDuration = 300; // milliseconds
+  // searchDebounceDuration must be at least 500ms to debounce working well
+  // I've tried 300ms and if you type fast, it will not work
+  static const int searchDebounceDuration = 500; // milliseconds
+
   static const int maxMobileWidth = 600;
 }
