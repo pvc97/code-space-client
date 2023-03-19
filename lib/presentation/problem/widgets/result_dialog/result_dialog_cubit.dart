c79ecff3ddfe47b0dd21dd090baf196974a70e0b
pvc2209 <cuongpv2209@gmail.com>
2023-03-19 16:11:27 +0700

    Emit event problem solve to course detail screen.

--- lib/presentation/problem/widgets/result_dialog/result_dialog_cubit.dart
+++ lib/presentation/problem/widgets/result_dialog/result_dialog_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:code_space_client/blocs/course_detail/course_detail_bloc.dart';
+import 'package:code_space_client/utils/event_bus/app_event.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,12 +8,25 @@ part 'result_dialog_state.dart';
 class ResultDialogCubit extends Cubit<ResultDialogState> {
   ResultDialogCubit() : super(ResultDialogState.initial());
 
-  void setTotalTestCases(int totalTestCases) {
-    emit(state.copyWith(totalTestCases: totalTestCases));
+  void setInitialData({
+    required String problemId,
+    required int totalTestCases,
+  }) {
+    emit(state.copyWith(problemId: problemId, totalTestCases: totalTestCases));
   }
 
   void addResult(bool result) {
     emit(state.copyWith(results: [...state.results, result]));
+
+    // Check if all test cases are correct
+    final correctAll = state.results.every((result) => result) &&
+        state.results.length == state.totalTestCases;
+
+    emit(state.copyWith(correctAll: correctAll));
+
+    if (correctAll) {
+      eventBus.fire(ProblemSolvedEvent(problemId: state.problemId));
+    }
   }
 
   void clearResults() {
--- lib/presentation/problem/widgets/result_dialog/result_dialog_state.dart
+++ lib/presentation/problem/widgets/result_dialog/result_dialog_state.dart
@@ -1,39 +1,41 @@
 part of 'result_dialog_cubit.dart';
 
 class ResultDialogState extends Equatable {
+  final String problemId;
   final int totalTestCases;
   final List<bool> results;
+  final bool correctAll;
 
   const ResultDialogState({
+    required this.problemId,
     required this.totalTestCases,
     required this.results,
+    required this.correctAll,
   });
 
   factory ResultDialogState.initial() {
     return const ResultDialogState(
+      problemId: '',
       totalTestCases: 0,
       results: [],
+      correctAll: false,
     );
   }
 
   @override
-  List<Object> get props => [totalTestCases, results];
+  List<Object?> get props => [problemId, totalTestCases, results, correctAll];
 
   ResultDialogState copyWith({
+    String? problemId,
     int? totalTestCases,
     List<bool>? results,
+    bool? correctAll,
   }) {
     return ResultDialogState(
+      problemId: problemId ?? this.problemId,
       totalTestCases: totalTestCases ?? this.totalTestCases,
       results: results ?? this.results,
+      correctAll: correctAll ?? this.correctAll,
     );
-  }
-
-  int get correctTestCases {
-    return results.where((result) => result).length;
-  }
-
-  bool get correctAll {
-    return correctTestCases == totalTestCases;
   }
 }
