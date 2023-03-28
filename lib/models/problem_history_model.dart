94641717ba431285cbe8759de55ac7b0c08bf9fb
pvc2209 <cuongpv2209@gmail.com>
2023-03-28 23:10:04 +0700

    Add problem history model.

---
+++ lib/models/problem_history_model.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'problem_history_model.g.dart';
+
+@JsonSerializable()
+class ProblemHistoryModel extends Equatable {
+  @JsonKey(name: 'id')
+  final String submissionId;
+  final String sourceCode;
+  final DateTime createdAt;
+  final int numberOfTestCases;
+  final int correctTestCases;
+  final int pointPerTestCase;
+
+  const ProblemHistoryModel({
+    required this.submissionId,
+    required this.sourceCode,
+    required this.createdAt,
+    required this.numberOfTestCases,
+    required this.correctTestCases,
+    required this.pointPerTestCase,
+  });
+
+  @override
+  List<Object?> get props => [
+        submissionId,
+        sourceCode,
+        createdAt,
+        numberOfTestCases,
+        correctTestCases,
+        pointPerTestCase,
+      ];
+
+  factory ProblemHistoryModel.fromJson(Map<String, dynamic> json) =>
+      _$ProblemHistoryModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProblemHistoryModelToJson(this);
+}
