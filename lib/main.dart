7bcc57264e5d3c95b4da7fde6d8b78519e14d3e5
pvc2209 <cuongpv2209@gmail.com>
2023-04-04 21:08:04 +0700

    Disable submit button before submissionId return from server.

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ void main() async {
 
   await Di.init();
   await AppConfigManager.init(
-    environmentType: EnvironmentType.prod,
+    environmentType: EnvironmentType.devWindowWeb,
     binding: binding,
   );
 
